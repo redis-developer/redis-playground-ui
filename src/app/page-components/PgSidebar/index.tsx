0f253cb36a988c6e02efc49293406eb5f3c14164
PrasanKumar <prasankumar93@gmail.com>
2024-12-24 17:56:42 +0530

    added docs, redis cloud link to sidebar

--- src/app/page-components/PgSidebar/index.tsx
+++ src/app/page-components/PgSidebar/index.tsx
@@ -9,8 +9,14 @@ import PgQueryTemplate from '../PgQueryTemplate';
 
 const labels = {
     configure: "CONFIGURE",
-    selectQuery: "SELECT QUERY"
+    selectQuery: "SELECT QUERY",
+    links: "LINKS"
 }
+
+const linkItems = [
+    { label: "Visit Docs", icon: "fa fa-book", url: "https://redis.io/docs/latest/" },
+    { label: "Redis Cloud (Free)", icon: "fa fa-cloud", url: "https://cloud.redis.io/" }
+]
 
 const PgSidebar = () => {
     const { queryTemplateData, selectedQuery, setSelectedQuery } = usePlaygroundContext();
@@ -33,43 +39,62 @@ const PgSidebar = () => {
 
     return <div className="pg-sidebar">
 
-        <div className="list">
-            <div className="list-title font-bold">
+        <div className="pg-list">
+            <div className="pg-list-title font-bold">
                 {labels.configure}
             </div>
-            <div className="list-item" onClick={() => setModalIsOpen(true)}>
-                <div className="list-item-icon">
+            <div className="pg-list-item" onClick={() => setModalIsOpen(true)}>
+                <div className="pg-list-item-icon">
                     <i className="fa fa-object-group"></i>
                 </div>
-                <div className="list-item-label">
+                <div className="pg-list-item-label">
                     {labels.selectQuery}
                 </div>
             </div>
         </div>
-        <div className="query-category list">
+
+        <div className="pg-query-category pg-list">
             {selectedCategory && (
-                <>
-                    <div className="list-title font-bold">
+                <div>
+                    <div className="pg-list-title font-bold">
                         {selectedCategory?.category}
                     </div>
                     <div>
                         {selectedCategory?.items.map(item => (
-                            <div className={`list-item ${item.queryId === selectedQuery?.queryId ? "list-item-active" : ""}`}
+                            <div className={`pg-list-item ${item.queryId === selectedQuery?.queryId ? "pg-list-item-active" : ""}`}
                                 key={item.label}
                                 onClick={() => handleQueryItemClick(item.queryId, selectedCategory?.category)}>
-                                <div className="list-item-icon">
+                                <div className="pg-list-item-icon">
                                     <i className="fa fa-arrow-right"></i>
                                 </div>
-                                <div className="list-item-label">
+                                <div className="pg-list-item-label">
                                     {item.label}
                                 </div>
                             </div>
                         ))}
                     </div>
-                </>
+                </div>
             )}
 
         </div>
+
+        <div className="pg-links pg-list">
+            <div className="pg-list-title font-bold">
+                {labels.links}
+            </div>
+            {linkItems.map(item => (
+                <div className="pg-list-item" key={item.label}>
+                    <div className="pg-list-item-icon">
+                        <i className={item.icon}></i>
+                    </div>
+                    <div className="pg-list-item-label" onClick={() => window.open(item.url, '_blank', 'noopener,noreferrer')}>
+                        {item.label}
+                    </div>
+                </div>
+            ))}
+        </div>
+
+
 
         <ModalPopup isOpen={modalIsOpen} onClose={() => setModalIsOpen(false)}>
 
@@ -77,7 +102,7 @@ const PgSidebar = () => {
 
         </ModalPopup>
 
-    </div>
+    </div >
 }
 
 export default PgSidebar;
