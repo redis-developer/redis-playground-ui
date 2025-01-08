8829863de990c16908a775be87f5a43371997d80
PrasanKumar <prasankumar93@gmail.com>
2025-01-09 01:34:30 +0530

    highlight search term in popup

--- src/app/page-components/PgQueryTemplate/index.tsx
+++ src/app/page-components/PgQueryTemplate/index.tsx
@@ -3,6 +3,7 @@ import './index.scss';
 import type { IQueryTemplateData } from "../usePlayground";
 
 import { useEffect, useState } from "react";
+import Highlighter from 'react-highlight-words';
 
 import { pgGetQueryNavbarData } from "@/app/utils/services";
 import { usePlaygroundContext } from "../PlaygroundContext";
@@ -15,12 +16,14 @@ interface IQueryTemplateProps {
 const labels = {
     searchPlaceholder: "Search queries..."
 }
+const highlightClassName = "pg-highlighted-text";
 
 const PgQueryTemplate = ({ onClose }: IQueryTemplateProps) => {
     const { setSelectedQuery, queryTemplateData, setQueryTemplateData, setApiCallInProgress } = usePlaygroundContext();
 
     const [filteredTemplateData, setFilteredTemplateData] = useState<IQueryTemplateData[]>(queryTemplateData);
     const [isShowLoader, setIsShowLoader] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
 
     useEffect(() => {
         if (queryTemplateData?.length === 0) {
@@ -49,16 +52,22 @@ const PgQueryTemplate = ({ onClose }: IQueryTemplateProps) => {
         }
     };
 
-    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        let searchValue = event?.target?.value;
-        searchValue = searchValue?.toLowerCase().trim() || "";
+    const handleClearSearch = () => {
+        setSearchValue("");
+        handleSearchChange(null);
+    };
 
-        if (searchValue) {
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement> | null) => {
+        let searchTerm = event?.target?.value || "";
+        setSearchValue(searchTerm);
+        searchTerm = searchTerm?.toLowerCase().trim();
+
+        if (searchTerm) {
             const filteredData = queryTemplateData.map((category) => ({
                 ...category,
                 items: category.items.filter((item) =>
-                    item.label.toLowerCase().includes(searchValue) ||
-                    item.description?.toLowerCase().includes(searchValue)
+                    item.label.toLowerCase().includes(searchTerm) ||
+                    item.description?.toLowerCase().includes(searchTerm)
                 )
             })).filter(category => category.items.length > 0);
             setFilteredTemplateData(filteredData);
@@ -72,7 +81,8 @@ const PgQueryTemplate = ({ onClose }: IQueryTemplateProps) => {
         <div className="query-search-bar-container">
             <div className="query-search-bar">
                 <i className="fa fa-search"></i>
-                <input type="text" placeholder={labels.searchPlaceholder} onChange={handleSearchChange} className="query-search-input" />
+                <input type="text" placeholder={labels.searchPlaceholder} onChange={handleSearchChange} className="query-search-input" value={searchValue} />
+                {searchValue && <i className="fa fa-times clear-icon" onClick={handleClearSearch}></i>}
             </div>
         </div>
         <Loader isShow={isShowLoader} />
@@ -80,12 +90,27 @@ const PgQueryTemplate = ({ onClose }: IQueryTemplateProps) => {
         <div className="query-category-container">
             {filteredTemplateData.map((category, index) => (
                 <div key={index} className="query-category">
-                    <div className="query-category-title font-bold">{category.category}</div>
+                    <div className="query-category-title font-bold">{category.category}
+                    </div>
                     <div className="query-category-items">
                         {category.items.map((item, index) => (
                             <div key={index} className="query-item" onClick={() => handleQueryItemClick(item.queryId, category.category)}>
-                                <div className="query-item-label font-bold">{item.label}</div>
-                                <div className="query-item-description">{item.description}</div>
+                                <div className="query-item-label font-bold">
+                                    <Highlighter
+                                        highlightClassName={highlightClassName}
+                                        searchWords={[searchValue]}
+                                        autoEscape={true}
+                                        textToHighlight={item.label}
+                                    />
+                                </div>
+                                <div className="query-item-description">
+                                    <Highlighter
+                                        highlightClassName={highlightClassName}
+                                        searchWords={[searchValue]}
+                                        autoEscape={true}
+                                        textToHighlight={item.description}
+                                    />
+                                </div>
                             </div>
                         ))}
                     </div>
