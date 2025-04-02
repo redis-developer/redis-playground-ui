4df190a18481b1c51647a9228722827837815a76
Cody Henshaw <cghensha@gmail.com>
2025-04-02 13:18:19 -0500

    add /try/sandbox to next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
+const isProd = process.env.NODE_ENV === "production";
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: false,//process.env.ANALYZE === 'true',
+module.exports = {
+  basePath: isProd ? "/try/sandbox" : "",
+};
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: false, //process.env.ANALYZE === 'true',
 });
 
 module.exports = withBundleAnalyzer({
-    swcMinify: true,  // Enable SWC minification
-    reactStrictMode: true,
-    output: "standalone", // lightweight standalone app 
+  swcMinify: true, // Enable SWC minification
+  reactStrictMode: true,
+  output: "standalone", // lightweight standalone app
 });
-
