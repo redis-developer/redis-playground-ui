8b537fe320a54d2694c85196fc8da8c0b7839620
Cody Henshaw <cghensha@gmail.com>
2025-04-02 13:38:57 -0500

    update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
-  basePath: isProd ? "/try/sandbox" : "",
-};
-
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: false, //process.env.ANALYZE === 'true',
 });
@@ -12,4 +8,5 @@ module.exports = withBundleAnalyzer({
   swcMinify: true, // Enable SWC minification
   reactStrictMode: true,
   output: "standalone", // lightweight standalone app
+  basePath: isProd ? "/try/sandbox" : "",
 });
