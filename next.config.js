1baeaba323097800b1b1a386049edef93012dc4d
Cody Henshaw <cghensha@gmail.com>
2025-04-21 14:48:21 -0500

    add basepath for production to work with routes

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: false, //process.env.ANALYZE === 'true',
 });
 
+const isProd = process.env.VERCEL_ENV === "production"; // or NODE_ENV === 'production'
+
 module.exports = withBundleAnalyzer({
-  swcMinify: true, // Enable SWC minification
+  swcMinify: true,
   reactStrictMode: true,
-  output: "standalone", // lightweight standalone app
+  output: "standalone",
+  basePath: isProd ? "/try/sandbox" : "",
+  assetPrefix: isProd ? "/try/sandbox/" : "",
 });
